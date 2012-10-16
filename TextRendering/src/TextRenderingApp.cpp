0f4371a60113e54c19e040077f53c10df4ba0363
Paul Houx <paul@cowlumbus.nl>
2012-10-16 19:00:38 +0200

    Improved mouse and keyboard interaction.

--- TextRendering/src/TextRenderingApp.cpp
+++ TextRendering/src/TextRenderingApp.cpp
@@ -18,13 +18,17 @@ public:
 	void draw();
 
 	void mouseDown( MouseEvent event );	
+	void mouseDrag( MouseEvent event );	
+	void mouseUp( MouseEvent event );
+	void mouseWheel( MouseEvent event );
 
 	void keyDown( KeyEvent event );
 
 	void resize( ResizeEvent event );
 	void fileDrop( FileDropEvent event );
 protected:
-	void updateWindowTitle();
+	Vec3f	constrainAnchor( const Vec3f &pt ) const;
+	void	updateWindowTitle();
 protected:
 	bool			mShowBounds;
 	bool			mShowWireframe;
@@ -35,60 +39,100 @@ protected:
 	//! 
 	TextBox			mTextBox;
 
-	//!
-	Vec2f			mOffset;
-	Vec2f			mPosition;
-	Vec2f			mTarget;
+	//! textbox transformation members
+	Vec3f			mAnchor;
+	Quatf			mOrientation;
+	float			mScale;
+	Matrix44f		mTransform;
+
+	//! textbox animation members
+	Vec3f			mAnchorTarget;
+	Vec3f			mAnchorSpeed;
+
+	//! interaction members
+	Vec3f			mInitialAnchor;
+	Quatf			mInitialOrientation;
+	float			mInitialScale;
+
+	Vec2f			mInitialMouse;
+	Vec2f			mCurrentMouse;
+
+	uint32_t		mMouseDownFrame;
+	uint32_t		mMouseDragFrame;
+
 };
 
 void TextRenderingApp::prepareSettings(Settings *settings)
 {
-	settings->setFrameRate(200.0f);
-	settings->setWindowSize(480, 720);
+	// set maximum frame rate quite high, so we can measure performance if vertical sync is disabled
+	settings->setFrameRate(500.0f);
+
+	// set initial window size
+	settings->setWindowSize(500, 750);
 }
 
 void TextRenderingApp::setup()
 {
+	// initialize member variables
 	mShowBounds = false;
 	mShowWireframe= false;
 
 	mFrontColor = Color::black();
 	mBackColor = Color::white();
 
-	mOffset = Vec2f(20, 20);
-	mPosition = mTarget = Vec2f(0, 0);
+	// set initial text position and scale
+	mScale = 1.0f;
+	mAnchor.x = 200.0f;
+	mAnchor.y = 250.0f;
+	mAnchorTarget = mAnchor = constrainAnchor( mAnchor );
+	mAnchorSpeed = Vec3f::zero();
 
 	try { 
 		// load fonts using the FontStore
-		fonts().loadFont( loadAsset("fonts/Walter Turncoat Regular.sdff") ); 
+		fonts().loadFont( loadAsset("fonts/Neucha Regular.sdff") ); 
 
 		// create a text box (rectangular text area)
-		mTextBox = TextBox( getWindowSize() );
+		mTextBox = TextBox( 400, 500 );
 		// set font and font size
-		mTextBox.setFont( fonts().getFont("Walter Turncoat Regular") );
-		mTextBox.setFontSize( 18.0f );
+		mTextBox.setFont( fonts().getFont("Neucha Regular") );
+		mTextBox.setFontSize( 24.0f );
 		// break lines between words
 		mTextBox.setBoundary( Text::WORD );
 		// adjust space between lines
-		mTextBox.setLineSpace( 1.2f );
+		mTextBox.setLineSpace( 1.1f );
 
-		// load a long text and hand it to the text box
+		// load a text and hand it to the text box
 		mTextBox.setText( loadString( loadAsset("fonts/readme.txt") ) );
 	}
 	catch( const std::exception & e ) { 
 		console() << e.what() << std::endl; 
 	}
 
+	// update the window title
 	updateWindowTitle();
 }
 
 void TextRenderingApp::shutdown()
 {
+	// no clean up necessary
 }
 
 void TextRenderingApp::update()
-{
-	mPosition += 0.15f * (mTarget - mPosition);
+{	
+	// if scrolling, gradually reduce speed and adjust target
+	mAnchorSpeed *= 0.95f;
+	mAnchorTarget -= mAnchorSpeed;
+	mAnchorTarget = constrainAnchor( mAnchorTarget );
+
+	// adjust anchor accordingly
+	mAnchor += 0.2f * (mAnchorTarget - mAnchor);
+
+	// calculate transformation matrix
+	mTransform.setToIdentity();
+	mTransform.translate( Vec3f( 0.5f * Vec2f( getWindowSize() ), 0.0f ) );	// position
+	mTransform *= mOrientation.toMatrix44();		// orientation
+	mTransform.scale( Vec3f(mScale, mScale, mScale) );						// scale
+	mTransform.translate( -mAnchor );				// anchor
 }
 
 void TextRenderingApp::draw()
@@ -99,37 +143,102 @@ void TextRenderingApp::draw()
 	// clear out the window
 	gl::clear( mBackColor );
 
-	// draw text box centered inside the window
-	//Area fit = Area::proportionalFit( Area(Vec2i::zero(), mTextBox.getSize()), getWindowBounds(), true, false ); 
-
+	// setup render states
 	gl::color( mFrontColor );
 	gl::enableAlphaBlending();
 
+	// apply transformations
 	gl::pushModelView();
-	//gl::translate( fit.getUL() );
-	gl::translate( mPosition + mOffset );
+	gl::multModelView( mTransform );
 
+	// draw the text
 	mTextBox.draw();
 
-	if(mShowWireframe) mTextBox.drawWireframe();
+	// draw mesh as wireframes if enabled
+	if(mShowWireframe) 
+		mTextBox.drawWireframe();
 	
+	// restore render states
 	gl::popModelView();
 	gl::disableAlphaBlending();
-
-	if(mShowBounds)
-		mTextBox.drawBounds( mPosition + mOffset );
-		//mTextBox.drawBounds( fit.getUL() );
 }
 
 void TextRenderingApp::mouseDown( MouseEvent event )
 {
+	// keep track of where we clicked the mouse
+	mCurrentMouse = mInitialMouse = event.getPos();
+
+	// keep track of current transformations
+	mInitialAnchor = mAnchor;
+	mInitialOrientation = mOrientation;
+	mInitialScale = mScale;
+
+	// make sure auto-scrolling is disabled
+	mAnchorTarget = mAnchor;	
+	mAnchorSpeed = Vec3f::zero();
+
+	// keep track of current frame, so we can calculate average drag speed in mouseUp()
+	mMouseDownFrame = getElapsedFrames();
+}
+
+void TextRenderingApp::mouseDrag( MouseEvent event )
+{
+	mCurrentMouse = event.getPos();
+
+	if( event.isLeftDown() )
+	{
+		// drag text up and down, limited by the window dimensions
+		Vec2f d( (mCurrentMouse.x - mInitialMouse.x) / mScale, (mCurrentMouse.y - mInitialMouse.y) / mScale );
+		mAnchor.x = mInitialAnchor.x - d.x;
+		mAnchor.y = mInitialAnchor.y - d.y;
+		mAnchorTarget = mAnchor = constrainAnchor( mAnchor );
+
+		// if anchor was limited, adjust initial anchor for better mouse response
+		mInitialAnchor.x = mAnchor.x + d.x;
+		mInitialAnchor.y = mAnchor.y + d.y;
+	
+		// keep track of current frame, so we can calculate average drag speed in mouseUp()
+		mMouseDragFrame = getElapsedFrames();
+	}
+	else if( event.isRightDown() )
+	{
+		// scale text
+		float d0 = mInitialMouse.length();
+		float d1 = mCurrentMouse.length();
+		mScale = mInitialScale * (d1 / d0);
+
+		// make sure auto-scrolling is disabled
+		mAnchorTarget = mAnchor = constrainAnchor( mAnchorTarget );
+		mAnchorSpeed = Vec3f::zero();
+	}
+}
+
+void TextRenderingApp::mouseUp( MouseEvent event )
+{
+	// don't calculate average speed if not dragged since more than 5 frames, or not dragged at all
+	if( (getElapsedFrames() - mMouseDragFrame) > 5 || mMouseDragFrame <= mMouseDownFrame )
+		mAnchorSpeed = Vec3f::zero();
+	else
+	{
+		mCurrentMouse = event.getPos();
+
+		// calculate average drag distance per frame
+		Vec2f d( (mCurrentMouse.x - mInitialMouse.x) / mScale, (mCurrentMouse.y - mInitialMouse.y) / mScale );
+		Vec2f avg = d / float( mMouseDragFrame - mMouseDownFrame );
+
+		mAnchorSpeed = Vec3f( avg, 0.0f );
+	}
+}
+
+void TextRenderingApp::mouseWheel( MouseEvent event )
+{		
+	// use the scroll wheel to scroll the text, two lines per event
+	mAnchorTarget.y -= event.getWheelIncrement() * mTextBox.getLeading() * 2.0f;
+	mAnchorSpeed = Vec3f::zero();
 }
 
 void TextRenderingApp::keyDown( KeyEvent event )
 {
-	int line = int( math<float>::floor( mTarget.y / mTextBox.getLeading() ) );
-	int max_lines = int( math<float>::floor( mTextBox.getBounds().getHeight() / mTextBox.getLeading() ) );
-
 	switch(event.getCode()) {
 	case KeyEvent::KEY_ESCAPE:
 		quit();
@@ -143,18 +252,8 @@ void TextRenderingApp::keyDown( KeyEvent event )
 	case KeyEvent::KEY_w:
 		mShowWireframe = !mShowWireframe;
 		break;
-	//case KeyEvent::KEY_b:
-	//	mShowBounds = !mShowBounds;
-	//	break;
-	case KeyEvent::KEY_PAGEDOWN:
-		line -= int( math<float>::floor( (getWindowHeight() - 40.0f) / mTextBox.getLeading() ) );
-		if( line < -max_lines ) line = -max_lines;
-		mTarget.y = line * mTextBox.getLeading();
-		break;
-	case KeyEvent::KEY_PAGEUP:
-		line += int( math<float>::floor( (getWindowHeight() - 40.0f) / mTextBox.getLeading() ) );
-		if( line > 0 ) line = 0;
-		mTarget.y = line * mTextBox.getLeading();
+	case KeyEvent::KEY_RETURN:
+		mScale = 1.0f;
 		break;
 	case KeyEvent::KEY_SPACE:
 		{ Color temp = mFrontColor; mFrontColor = mBackColor; mBackColor = temp; }
@@ -162,14 +261,14 @@ void TextRenderingApp::keyDown( KeyEvent event )
 	case KeyEvent::KEY_LEFTBRACKET:
 		if(mTextBox.getFontSize() > 0.5f)
 			mTextBox.setFontSize( mTextBox.getFontSize() - 0.5f );
-		mTarget.y = line * mTextBox.getLeading();
 		break;
 	case KeyEvent::KEY_RIGHTBRACKET:
 		mTextBox.setFontSize( mTextBox.getFontSize() + 0.5f );
-		mTarget.y = line * mTextBox.getLeading();
 		break;
 	case KeyEvent::KEY_LEFT:
-		if( mTextBox.getAlignment() == TextBox::RIGHT )
+		/*if( mTextBox.getAlignment() == TextBox::JUSTIFIED )
+			mTextBox.setAlignment( TextBox::RIGHT );
+		else*/ if( mTextBox.getAlignment() == TextBox::RIGHT )
 			mTextBox.setAlignment( TextBox::CENTER );
 		else if( mTextBox.getAlignment() == TextBox::CENTER )
 			mTextBox.setAlignment( TextBox::LEFT );
@@ -179,16 +278,26 @@ void TextRenderingApp::keyDown( KeyEvent event )
 			mTextBox.setAlignment( TextBox::CENTER );
 		else if( mTextBox.getAlignment() == TextBox::CENTER )
 			mTextBox.setAlignment( TextBox::RIGHT );
+		/*else if( mTextBox.getAlignment() == TextBox::RIGHT )
+			mTextBox.setAlignment( TextBox::JUSTIFIED );*/
 		break;
 	case KeyEvent::KEY_UP:
 		if( mTextBox.getLineSpace() > 0.2f )
 			mTextBox.setLineSpace( mTextBox.getLineSpace() - 0.1f );
-		mTarget.y = line * mTextBox.getLeading();
 		break;
 	case KeyEvent::KEY_DOWN:
 		if( mTextBox.getLineSpace() < 5.0f )
 			mTextBox.setLineSpace( mTextBox.getLineSpace() + 0.1f );
-		mTarget.y = line * mTextBox.getLeading();
+		break;
+	case KeyEvent::KEY_PAGEDOWN:
+		// TODO: don't scroll if mouse down
+		mAnchorTarget.y += (getWindowHeight() / mScale) - mTextBox.getLeading();
+		mAnchorSpeed = Vec3f::zero();
+		break;
+	case KeyEvent::KEY_PAGEUP:
+		// TODO: don't scroll if mouse down
+		mAnchorTarget.y -= (getWindowHeight() / mScale) - mTextBox.getLeading();
+		mAnchorSpeed = Vec3f::zero();
 		break;
 	}
 
@@ -198,12 +307,10 @@ void TextRenderingApp::keyDown( KeyEvent event )
 
 void TextRenderingApp::resize( ResizeEvent event )
 {
-	// allow 20 pixels margin
-	Area box( Vec2i::zero(), event.getSize() );
-	box.expand(-20, -20);
-	box.setY2( box.getY1() );
+	// resize text box with a margin of 20 pixels on each side
+	mTextBox.setSize( event.getWidth() - 40.0f, 0.0f );
 
-	mTextBox.setSize( box.getSize() );
+	mAnchor = mAnchorTarget = constrainAnchor( mAnchorTarget );
 }
 
 void TextRenderingApp::fileDrop( FileDropEvent event )
@@ -271,6 +378,28 @@ void TextRenderingApp::fileDrop( FileDropEvent event )
 	}
 
 	updateWindowTitle();
+}
+
+Vec3f TextRenderingApp::constrainAnchor( const Vec3f &pt ) const
+{
+	Vec2f	margin = Vec2f(20, 20) / mScale;
+	Vec2f	size = mTextBox.getBounds().getSize();
+	Vec2f	center = 0.5f / mScale * Vec2f(getWindowSize());
+
+	float x = math<float>::min( center.x - margin.x, 0.5f * size.x );
+	float y = math<float>::min( center.y - margin.y, 0.5f * size.y );
+
+	float minx = math<float>::min( size.x - x, x );
+	float maxx = math<float>::max( size.x - x, x );
+	float miny = math<float>::min( size.y - y, y );
+	float maxy = math<float>::max( size.y - y, y );
+
+	Vec3f result;
+	result.x = math<float>::clamp( pt.x, minx, maxx );
+	result.y = math<float>::clamp( pt.y, miny, maxy );
+	result.z = pt.z;
+	
+	return result;
 }
 
 void TextRenderingApp::updateWindowTitle()
