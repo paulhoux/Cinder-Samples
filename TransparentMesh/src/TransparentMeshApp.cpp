af96c90691746fddec113d4a8eed7c55f009e15e
Paul Houx <phoux@barbariangroup.com>
2014-10-21 14:24:08 -0400

    Updated TransparentMesh sample.

--- TransparentMesh/src/TransparentMeshApp.cpp
+++ TransparentMesh/src/TransparentMeshApp.cpp
@@ -5,10 +5,10 @@
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and
-	the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-	the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+ the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ the following disclaimer in the documentation and/or other materials provided with the distribution.
 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
@@ -18,12 +18,15 @@
  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #include "cinder/ImageIo.h"
 #include "cinder/MayaCamUI.h"
 #include "cinder/app/AppBasic.h"
+#include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Context.h"
+#include "cinder/gl/GlslProg.h"
 #include "cinder/gl/Texture.h"
 
 using namespace ci;
@@ -33,43 +36,53 @@ using namespace std;
 class TransparentMeshApp : public AppBasic {
 public:
 	void prepareSettings( Settings *settings );
-	
+
 	void setup();
 	void update();
 	void draw();
-	
+
 	void resize();
-	
-	void mouseMove( MouseEvent event );	
-	void mouseDown( MouseEvent event );	
-	void mouseDrag( MouseEvent event );	
-	void mouseUp( MouseEvent event );	
-	
+
+	void mouseMove( MouseEvent event );
+	void mouseDown( MouseEvent event );
+	void mouseDrag( MouseEvent event );
+	void mouseUp( MouseEvent event );
+
 	void keyDown( KeyEvent event );
 	void keyUp( KeyEvent event );
 private:
 	// our textures
-	gl::Texture	mTexEarth;
-	gl::Texture	mTexBackground;
+	gl::TextureRef	mTexEarth;
+	gl::TextureRef	mTexBackground;
+
+	// our shader
+	gl::GlslProgRef	mShader;
 
 	// animate the Earth by rotating it around y-axis
-	float		mDegrees;
+	float			mDegrees;
 
 	// our controlable 3D camera
-	MayaCamUI	mCamera;
+	MayaCamUI		mCamera;
 };
 
-void TransparentMeshApp::prepareSettings(Settings *settings)
+void TransparentMeshApp::prepareSettings( Settings *settings )
 {
-	settings->setTitle("Transparent Mesh Sample");
+	settings->setTitle( "Transparent Mesh Sample" );
 }
 
 void TransparentMeshApp::setup()
 {
-	// load our textures
+	// load our textures and shader
 	try {
-		mTexEarth = gl::Texture( loadImage( loadAsset("earth.png") ) );
-		mTexBackground = gl::Texture( loadImage( loadAsset("background.png") ) );
+		// by enabling mip-mapping, we can reduce aliasing near the edges of the sphere
+		gl::Texture2d::Format fmt;
+		fmt.enableMipmapping( true );
+		fmt.setMinFilter( GL_LINEAR_MIPMAP_LINEAR );
+
+		mTexEarth = gl::Texture::create( loadImage( loadAsset( "earth.png" ) ), fmt );
+		mTexBackground = gl::Texture::create( loadImage( loadAsset( "background.png" ) ) );
+
+		mShader = gl::context()->getStockShader( gl::ShaderDef().color().texture() );
 	}
 	catch( const std::exception &e ) {
 		console() << e.what() << std::endl;
@@ -82,8 +95,8 @@ void TransparentMeshApp::setup()
 	// intialize 3D camera
 	CameraPersp cam;
 	cam.setPerspective( 60.0f, 1.0f, 0.1f, 1000.0f );
-	cam.setEyePoint( Vec3f(0, 150, -200) );
-	cam.setCenterOfInterestPoint( Vec3f::zero() );
+	cam.setEyePoint( vec3( 0, 150, -200 ) );
+	cam.setCenterOfInterestPoint( vec3( 0 ) );
 
 	mCamera.setCurrentCam( cam );
 }
@@ -91,12 +104,12 @@ void TransparentMeshApp::setup()
 void TransparentMeshApp::update()
 {
 	// rotate the Earth
-	mDegrees += 0.25f;
+	mDegrees += glm::radians( 0.25f );
 }
 
 void TransparentMeshApp::draw()
 {
-	gl::clear(); 
+	gl::clear();
 
 	// first draw a 2D background
 	gl::color( Color::white() );
@@ -110,10 +123,11 @@ void TransparentMeshApp::draw()
 	gl::setMatrices( mCamera.getCamera() );
 
 	// rotate the coordinate system, so the earth rotates over time
-	gl::rotate( mDegrees * Vec3f::yAxis() );
+	gl::rotate( mDegrees, 0, 1, 0 );
 
-	// bind the earth texture (containing transparency) and enable alpha blending
-	mTexEarth.enableAndBind();
+	// bind the shader and earth texture (containing transparency) and enable alpha blending
+	gl::ScopedGlslProg shader( mShader );
+	gl::ScopedTextureBind tex0( mTexEarth );
 	gl::enableAlphaBlending();
 
 	// the trick with rendering transparent objects, is to render from back to front.
@@ -123,23 +137,20 @@ void TransparentMeshApp::draw()
 
 	// draw all back-facing polygons first (in a slightly darker color)
 	glCullFace( GL_FRONT );
-	gl::color( Color(0.75f, 0.75f, 0.75f) );
-	gl::drawSphere( Vec3f::zero(), 100.0f, 60 );
+	gl::color( Color( 0.75f, 0.75f, 0.75f ) );
+	gl::drawSphere( vec3( 0 ), 100.0f, 60 );
 
 	// draw all front-facing polygons next (in their original color)
 	glCullFace( GL_BACK );
-	gl::color( Color(1, 1, 1) );
-	gl::drawSphere( Vec3f::zero(), 100.0f, 60 );
+	gl::color( Color( 1, 1, 1 ) );
+	gl::drawSphere( vec3( 0 ), 100.0f, 60 );
 
 	// disable face culling again
 	gl::disable( GL_CULL_FACE );
 
 	// restore all the other render states
 	gl::disableAlphaBlending();
-	mTexEarth.unbind();
-
 	gl::popMatrices();
-
 	gl::disableDepthWrite();
 	gl::disableDepthRead();
 }
