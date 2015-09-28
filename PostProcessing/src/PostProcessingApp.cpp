15221b36994ea28cadde48e032240d0137ae07d8
Paul Houx <github@cowlumbus.nl>
2015-09-28 13:52:31 -0400

    Removed video player protion of PostProcessing sample. Will be added back once Cinder provides proper video support again.

--- PostProcessing/src/PostProcessingApp.cpp
+++ PostProcessing/src/PostProcessingApp.cpp
@@ -20,80 +20,63 @@
  POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma comment(lib, "QTMLClient.lib")
-#pragma comment(lib, "CVClient.lib")
-
 #include "cinder/Filesystem.h"
 #include "cinder/ImageIo.h"
 #include "cinder/Matrix.h"
 #include "cinder/Surface.h"
 #include "cinder/Utilities.h"
 
-#include "cinder/app/AppBasic.h"
+#include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 
 #include "cinder/gl/Fbo.h"
 #include "cinder/gl/GlslProg.h"
 #include "cinder/gl/Texture.h"
-#include "cinder/qtime/QuickTime.h"
+#include "cinder/gl/draw.h"
+#include "cinder/gl/scoped.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-class PostProcessingApp : public AppBasic {
+class PostProcessingApp : public App {
 public:
-	void prepareSettings( Settings *settings );
-	void setup();
-	void update();
-	void draw();
+	static void prepare( Settings *settings );
 
-	void keyDown( KeyEvent event );
-	void fileDrop( FileDropEvent event );
+	void setup() override;
+	void draw() override;
 
-	void play( const fs::path &path );
-	void playNext();
+	void keyDown( KeyEvent event ) override;
+	void fileDrop( FileDropEvent event ) override;
+
 protected:
 	gl::TextureRef			mImage;
 	gl::GlslProgRef			mShader;
-	qtime::MovieSurfaceRef	mMovie;
+
 	fs::path				mFile;
 };
 
-void PostProcessingApp::prepareSettings( Settings *settings )
+void PostProcessingApp::prepare( Settings *settings )
 {
 	settings->setWindowSize( 1024, 768 );
-	settings->setFrameRate( 30.0f );
-	settings->setTitle( "Post-processing Video Player" );
+	settings->disableFrameRate();
+	settings->setTitle( "Post-processing" );
 }
 
 void PostProcessingApp::setup()
 {
 	// load test image
-	try { mImage = gl::Texture::create( loadImage( loadAsset( "test.png" ) ) ); }
-	catch( const std::exception &e ) { console() << "Could not load image: " << e.what() << std::endl; }
+	try {
+		mImage = gl::Texture::create( loadImage( loadAsset( "test.png" ) ) );
+	}
+	catch( const std::exception &e ) {
+		console() << "Could not load image: " << e.what() << std::endl;
+	}
 
 	// load post-processing shader
 	//  adapted from a shader by Iñigo Quílez ( http://www.iquilezles.org/ )
 	try { mShader = gl::GlslProg::create( loadAsset( "post_process.vert" ), loadAsset( "post_process.frag" ) ); }
 	catch( const std::exception &e ) { console() << "Could not load & compile shader: " << e.what() << std::endl; quit(); }
-}
-
-void PostProcessingApp::update()
-{
-	// update movie texture if necessary
-	if( mMovie ) {
-		// get movie surface
-		Surface surf = mMovie->getSurface();
-
-		// copy surface into texture
-		if( surf )
-			mImage = gl::Texture::create( surf );
-
-		// play next movie in directory when done
-		if( mMovie->isDone() )
-			playNext();
-	}
 }
 
 void PostProcessingApp::draw()
@@ -104,9 +87,9 @@ void PostProcessingApp::draw()
 	// bind shader and set shader variables
 	gl::ScopedGlslProg shader( mShader );
 	mShader->uniform( "tex0", 0 );
-	mShader->uniform( "time", (float) getElapsedSeconds() );
+	mShader->uniform( "time", (float)getElapsedSeconds() );
 
-	// draw image or video
+	// draw image
 	gl::ScopedTextureBind tex0( mImage );
 
 	gl::color( Color::white() );
@@ -116,12 +99,12 @@ void PostProcessingApp::draw()
 void PostProcessingApp::keyDown( KeyEvent event )
 {
 	switch( event.getCode() ) {
-	case KeyEvent::KEY_ESCAPE:
-		quit();
-		break;
-	case KeyEvent::KEY_f:
-		setFullScreen( !isFullScreen() );
-		break;
+		case KeyEvent::KEY_ESCAPE:
+			quit();
+			break;
+		case KeyEvent::KEY_f:
+			setFullScreen( !isFullScreen() );
+			break;
 	}
 }
 
@@ -134,60 +117,9 @@ void PostProcessingApp::fileDrop( FileDropEvent event )
 		// try loading image file
 		mImage = gl::Texture::create( loadImage( mFile ) );
 	}
-	catch( ... ) {
-		// otherwise, try loading QuickTime video
-		play( mFile );
+	catch( const std::exception &e ) {
+		console() << "Could not load image: " << e.what() << std::endl;
 	}
 }
 
-void PostProcessingApp::play( const fs::path &path )
-{
-	try {
-		// try loading QuickTime movie
-		mMovie = qtime::MovieSurface::create( path );
-		mMovie->play();
-	}
-	catch( ... ) {}
-
-	// keep track of file
-	mFile = path;
-}
-
-void PostProcessingApp::playNext()
-{
-	// get directory
-	fs::path path = mFile.parent_path();
-
-	// list *.mov files
-	vector<string> files;
-	string filter( ".mov" );
-
-	fs::directory_iterator end_itr;
-	for( fs::directory_iterator itr( path ); itr != end_itr; ++itr ) {
-		// skip if not a file
-		if( !boost::filesystem::is_regular_file( itr->status() ) ) continue;
-
-		// skip if no match
-		if( itr->path().filename().string().find( filter ) == string::npos ) continue;
-
-		// file matches, store it
-		files.push_back( itr->path().string() );
-	}
-
-	// check if playable files are found
-	if( files.empty() ) return;
-
-	// play next file
-	vector<string>::iterator itr = find( files.begin(), files.end(), mFile );
-	if( itr == files.end() ) {
-		play( files[0] );
-	}
-	else {
-		++itr;
-		if( itr == files.end() )
-			play( files[0] );
-		else play( *itr );
-	}
-}
-
-CINDER_APP_BASIC( PostProcessingApp, RendererGl )
+CINDER_APP( PostProcessingApp, RendererGl, &PostProcessingApp::prepare )
